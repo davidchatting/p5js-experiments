8835cf7c695eae5ea9e668ce675cbce2d8a9a228
David Chatting <david@davidchatting.com>
2023-01-28 17:52:18 +0000

    tidy-up

--- mediapipe-2dsegmentation-staticimage/sketch.js
+++ mediapipe-2dsegmentation-staticimage/sketch.js
@@ -3,7 +3,7 @@
 //David Chatting - davidchatting.com -  31st December 2022
 
 var canvas;
-var frame = null;
+var inputImage = null;
 var segmentedFrame = null;
 var segmentationCanvas = null;
 var bodySegmenter = null;
@@ -13,8 +13,9 @@ var foundBodies = [];
 function setup() {
   p5.disableFriendlyErrors = true;
 
-  canvas = createCanvas(800, 800, WEBGL);
+  canvas = createCanvas(800, 800);
   canvas.parent("p5jsCanvas");
+  
   canvas.drop(onFileDropped);
   createForegroundSegmenter();
 }
@@ -35,19 +36,22 @@ async function createForegroundSegmenter() {
 
 function onFileDropped(file) {
   console.log("onFileDropped");
-  frame = createImg(file.data, "alt", "", findForeground);
-  //frame.addClass('data-img');
+  inputImage = createImg(file.data, "alt", "", function () {
+    findForeground(inputImage.elt);
+    inputImage.remove();
+  } );
+  inputImage.elt.classList.add('hide');  //because we need to keep the dimensions - can't use inputImage.hide()
 }
 
 function draw() {
   background(200);
-  orbitControl();  //allow 3d control of the image
   
-  if(frame) {
+  if(inputImage) {
     push();
-    scale(min(width/frame.width, height/frame.height));  //scale the image to fit the canvas
-    translate(-frame.width/2, -frame.height/2);          //centre the image
-    image(frame, 0, 0, frame.width, frame.height);
+    translate(width/2, height/2); 
+    scale(min(width/inputImage.width, height/inputImage.height));  //scale the image to fit the canvas
+    translate(-inputImage.width/2, -inputImage.height/2);          //centre the image
+    image(inputImage, 0, 0, inputImage.width, inputImage.height);
     if(segmentedFrame) {
       push();
       translate(segmentedFrame.width, 0);
@@ -55,27 +59,24 @@ function draw() {
       image(segmentedFrame, 0, 0);
       pop();
     }
-    drawBodies(foundBodies);
     pop();
   }
 }
 
-async function findForeground() {
+async function findForeground(imageElement) {
   console.log("findForeground");
   segmentationCanvas = document.createElement('canvas');
   segmentationCanvas.classList.add('mediapipe-canvas');
   
-  segmentationCanvas.width = frame.elt.width;
-  segmentationCanvas.height = frame.elt.height;
+  segmentationCanvas.width = imageElement.width;
+  segmentationCanvas.height = imageElement.height;
   document.body.appendChild(segmentationCanvas);
   
-  await selfieSegmentation.send({ image: frame.elt });
+  await selfieSegmentation.send({ image: imageElement });
 }
 
 function onFindForegroundResults(results) {
-  console.log(results);
-  console.log(results.segmentationMask);
-  
+  console.log("onFindForegroundResults");
   const canvasCtx = segmentationCanvas.getContext('2d');
   canvasCtx.drawImage(results.segmentationMask, 0, 0, segmentationCanvas.width, segmentationCanvas.height);
   const imageData = canvasCtx.getImageData(0, 0, segmentationCanvas.width, segmentationCanvas.height);
@@ -88,18 +89,3 @@ function onFindForegroundResults(results) {
   }
   segmentedFrame.updatePixels();
 }
-
-function drawBodies(bodies){
-  fill(255,255,255);
-  stroke(255,255,255);
-  
-  for (var i = 0; i < bodies.length; i++){
-    console.log(bodies[i]);
-//     var hand = hands[i].keypoints;
-
-//     for (var j = 0; j < hand.length; j++){
-//       point(hand[j].x, hand[j].y);  //correcting the orientation of the hands in z
-//     }
-  }
-}
-
